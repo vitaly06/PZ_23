17a08abcf3acbae1174231f9375f3107a3b267f0
vitaly06 <94614764+vitaly06@users.noreply.github.com>
2024-05-06 20:51:24 +0500

    All

---
+++ Sadikov_23/Sadikov_23/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Sadikov_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Hardware> hardwares = new List<Hardware>();
+            Monitor monitor = new Monitor();
+
+            for (int i = 1; i < 4; i++)
+            {
+                hardwares.Add(new Hardware(i));
+            }
+
+            foreach (Hardware hardware in hardwares)
+            {
+                hardware.CritChargeLevelEvent += monitor.HardwareOff;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                hardwares[0].OnPower();
+                hardwares[1].OnPower();
+                hardwares[2].OnPower();
+            }
+        }
+    }
+}
