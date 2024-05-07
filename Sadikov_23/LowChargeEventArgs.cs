b9639308a8c389ff31abe73638833416466576f0
vitaly06 <94614764+vitaly06@users.noreply.github.com>
2024-05-07 08:19:43 +0300

    add files

---
+++ Sadikov_23/LowChargeEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sadikov_23
+{
+    internal class LowChargeEventArgs : EventArgs
+    {
+        public string Message { get; set; }
+        public LowChargeEventArgs(string message) => Message = message;
+
+        public override string ToString()
+        {
+            return Message;
+        }
+    }
+}
